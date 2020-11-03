7631a3ee70176be3f517ce689b3a9137e22f1ebf
Hector Lopez <hectorvmlopez@gmail.com>
2020-11-03 02:04:35 -0500

    adding Contact Section, missing stickit navbar and put photos and links on projects

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,13 +9,45 @@ const Contact: React.FunctionComponent<ContactProps> = () => {
     return ( 
         <div className="container">
             <h2 className="about-title">
-                About
+                Contact
             </h2>
             <div className="row">
                 <div className="col-lg-8 mt-5 mt-lg-0">
+                    <div className="info d-flex justify-content-center align-items-center">
+                        
+                        <div className="location d-flex">
+                            <i className="icofont-location-pin"></i>
+                                <div className="info-text ">   
+                                    <h4 className="d-block">Location:</h4>
+                                    <p>Barranquilla, Colombia</p>
+                                </div>
+                        </div>
+
+                        <div className="Email d-flex mt-x">
+                                <i className="icofont-email"></i>
+                                <div className="info-text">   
+                                    <h4 className="d-block">Email:</h4>
+                                    <p>hectorvmlopez@gmail</p>
+                                </div>
+                        </div>
+
+
+                        <div className="Call d-flex mt-x mb-x">
+                                <i className="icofont-ui-cell-phone"></i>
+                                <div className="info-text">   
+                                    <h4 className="d-block">Call:</h4>
+                                    <p>+57 3205552871</p>
+                                </div>
+                        </div>
+
+                    </div>
+                        
+
+
                 </div>
+
                 <div className="col-lg-4">
-                    <img src={Foto} className="img-fluid rounded border rounded-circle" alt="Responsive "/>
+                    <img src={Foto} className="img-fluid border rounded-circle mx-auto d-block" alt="Responsive "/>
                 </div>
             </div>
             <div className="row">
